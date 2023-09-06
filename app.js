8a1a6f155afed3c1b447cc7f39161b333ecd247f
Bolsarim <giovanna.bolsarim@gmail.com>
2023-09-05 21:56:39 -0300

    calculo de média

---
+++ app.js
@@ -0,0 +1,23 @@
+/* 
+alert("Oi, isso é uma mensagem") // comando de saida de dados 
+
+console.log("Olá, isso é outra mensagem")
+
+alert(10/5) // fazer aparecer a conta no arquivo
+
+console.log(10/5) // fazer a conta aparecer no arquivo
+= é operador de atribuição, ele pega de um lado e coloca do outro
+*/
+
+nota1 = 10 
+nota2 = 8
+nota3 = 5
+nota4 = 8
+
+console.log("A média do João é: ")
+//console.log((nota1 + nota2 + nota3 + nota4)/4)
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+console.log(media)
+
